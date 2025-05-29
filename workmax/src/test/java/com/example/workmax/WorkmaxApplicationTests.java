e892c2d7dfbe319b7fb92c80382a1bef7e3ffced
lakshaykadam <lakshaykadam@google.com>
2025-05-29 11:38:50 +0530

    setup intial backend

---
+++ workmax/src/test/java/com/example/workmax/WorkmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
